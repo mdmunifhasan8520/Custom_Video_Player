b6136119e4cf1c43e20f6d6274578ceb9acf41ca
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-05-07 12:49:59 +0600

    play a local video file.

--- Custom_Video_Player/ViewController.swift
+++ Custom_Video_Player/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var customVideoView: UIView!
+    
+    
+    var player: AVPlayer!
+    var avpController = AVPlayerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func playBtn(_ sender: Any) {
+        let filepath: String? = Bundle.main.path(forResource: "video", ofType: "mp4")
+        let fileURL = URL.init(fileURLWithPath: filepath!)
+        
+        // Setup AVPlayer
+        player = AVPlayer(url: fileURL)
+        
+        avpController.player = player
+        
+        avpController.view.frame.size.height = customVideoView.frame.size.height
+        
+        avpController.view.frame.size.width = customVideoView.frame.size.width
+        
+        self.customVideoView.addSubview(avpController.view)
+        player.play()
+    }
+    
 }
 
