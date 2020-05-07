0a88309eefdcfa0334d081756b2ce4145276fe7d
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-05-07 11:22:05 +0600

    Initial Commit

---
+++ Custom_Video_Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom_Video_Player
+//
+//  Created by Mac PC on 5/7/20.
+//  Copyright © 2020 Ticon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Custom_Video_PlayerTests/Custom_Video_PlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Custom_Video_PlayerTests.swift
+//  Custom_Video_PlayerTests
+//
+//  Created by Mac PC on 5/7/20.
+//  Copyright © 2020 Ticon. All rights reserved.
+//
+
+import XCTest
+@testable import Custom_Video_Player
+
+class Custom_Video_PlayerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
